7b98998df6797f84b451ba942391109daa78af92
jean luc kawel <20ka208@esisalama.org>
2024-01-30 22:49:52 +0200

    fix header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,60 @@
-@include('App.app')
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Agrica - Organic Farm Agriculture Template">
 
-<!-- Header
-============================================= -->
+    <!-- ========== Page Title ========== -->
+    <title>Agrica - Organic Farm Agriculture Template</title>
+
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/magnific-popup.css" rel="stylesheet">
+    <link href="assets/css/flaticon-set.css" rel="stylesheet">
+    <link href="assets/css/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/animate.min.css" rel="stylesheet">
+    <link href="assets/css/validnavs.css" rel="stylesheet">
+    <link href="assets/css/helper.css" rel="stylesheet">
+    <link href="assets/css/unit-test.css" rel="stylesheet">
+    <link href="assets/css/shop.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- ========== End Stylesheet ========== -->
+
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+</head>
+<body>
+<div id="preloader">
+    <div id="agrica-preloader" class="agrica-preloader">
+        <div class="animation-preloader">
+            <div class="spinner"></div>
+        </div>
+        <div class="loader">
+            <div class="row">
+                <div class="col-3 loader-section section-left">
+                    <div class="bg"></div>
+                </div>
+                <div class="col-3 loader-section section-left">
+                    <div class="bg"></div>
+                </div>
+                <div class="col-3 loader-section section-right">
+                    <div class="bg"></div>
+                </div>
+                <div class="col-3 loader-section section-right">
+                    <div class="bg"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <header>
     <!-- Start Navigation -->
     <nav class="navbar mobile-sidenav inc-shape navbar-sticky navbar-default validnavs dark">
@@ -32,26 +85,25 @@
 
                 <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
                     <li class="dropdown">
-                        <a href="#" class="active" data-toggle="dropdown" >Accueil</a>
+                        <a href="#" class="active"  >Accueil</a>
                     </li>
                     <li class="dropdown">
-                        <a href="#"  data-toggle="dropdown" >Mission</a>
+                        <a href="#"   >Mission</a>
                     </li>
                     <li class="dropdown">
-                        <a href="project.html"  data-toggle="dropdown" >Objectifs</a>
+                        <a href="project.html"   >Project</a>
                     </li>
                     <li class="dropdown">
-                        <a href="#"  data-toggle="dropdown" >Realisation</a>
+                        <a href="#"   >Objectif</a>
                     </li>
                     <li class="dropdown">
-                        <a href="#"  data-toggle="dropdown" >Contact</a>
+                        <a href="#" >Expertise</a>
                     </li>
                     <li class="dropdown">
-                        <a href="#" data-toggle="dropdown" >Partage d'experience</a>
+                        <a href="#"   >Réalisations</a>
                     </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
-
 
 
             <!-- Main Nav -->
@@ -63,4 +115,4 @@
     </nav>
     <!-- End Navigation -->
 </header>
-<!-- End Header -->
+</body>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,67 +3,15 @@
 
 
 <!-- Mirrored from validthemes.net/site-template/agrica/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Jan 2024 11:10:38 GMT -->
-<head>
-    <!-- ========== Meta Tags ========== -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Agrica - Organic Farm Agriculture Template">
 
-    <!-- ========== Page Title ========== -->
-    <title>Agrica - Organic Farm Agriculture Template</title>
-
-    <!-- ========== Favicon Icon ========== -->
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
-
-    <!-- ========== Start Stylesheet ========== -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/css/magnific-popup.css" rel="stylesheet">
-    <link href="assets/css/flaticon-set.css" rel="stylesheet">
-    <link href="assets/css/swiper-bundle.min.css" rel="stylesheet">
-    <link href="assets/css/animate.min.css" rel="stylesheet">
-    <link href="assets/css/validnavs.css" rel="stylesheet">
-    <link href="assets/css/helper.css" rel="stylesheet">
-    <link href="assets/css/unit-test.css" rel="stylesheet">
-    <link href="assets/css/shop.css" rel="stylesheet">
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-    <!-- ========== End Stylesheet ========== -->
-
-    <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-    <![endif]-->
-
-</head>
 
 <body>
 
+@include('layouts.header')
+
 <!-- Start Preloader
 ============================================= -->
-<div id="preloader">
-    <div id="agrica-preloader" class="agrica-preloader">
-        <div class="animation-preloader">
-            <div class="spinner"></div>
-        </div>
-        <div class="loader">
-            <div class="row">
-                <div class="col-3 loader-section section-left">
-                    <div class="bg"></div>
-                </div>
-                <div class="col-3 loader-section section-left">
-                    <div class="bg"></div>
-                </div>
-                <div class="col-3 loader-section section-right">
-                    <div class="bg"></div>
-                </div>
-                <div class="col-3 loader-section section-right">
-                    <div class="bg"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!-- End Preloader -->
 
 <!-- Start Header Top
@@ -73,66 +21,7 @@
 
 <!-- Header
 ============================================= -->
-<header>
-    <!-- Start Navigation -->
-    <nav class="navbar mobile-sidenav inc-shape navbar-sticky navbar-default validnavs dark">
 
-        <div class="container d-flex justify-content-between align-items-center">
-
-
-            <div class="navbar-brand-left">
-                <!-- Start Header Navigation -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <a class="navbar-brand" href="index.html">
-                        <img src="assets/img/logo-mix.png" class="logo" alt="Logo">
-                    </a>
-                </div>
-                <!-- End Header Navigation -->
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-menu">
-
-                <img src="assets/img/logo.png" alt="Logo">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-                    <i class="fa fa-times"></i>
-                </button>
-
-                <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
-                    <li class="dropdown">
-                        <a href="#" class="active"  >Accueil</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#"   >Mission</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="project.html"   >Project</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#"   >Objectif</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" >Expertise</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#"   >Réalisations</a>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-
-
-            <!-- Main Nav -->
-        </div>
-        <!-- Overlay screen for menu -->
-        <div class="overlay-screen"></div>
-        <!-- End Overlay screen for menu -->
-
-    </nav>
-    <!-- End Navigation -->
-</header>
 <!-- End Header -->
 
 <!-- Start Banner Area
