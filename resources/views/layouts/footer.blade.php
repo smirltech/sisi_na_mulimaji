7553bb60b44b32b436312716e954a5e30a039951
jean luc kawel <20ka208@esisalama.org>
2024-01-31 19:20:52 +0200

    add realisation

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,136 @@
+<footer class="bg-dark text-light">
+    <div class="container">
+        <div class="f-items default-padding">
+            <div class="row">
+
+                <!-- Single Itme -->
+                <div class="col-lg-4 col-md-6 item">
+                    <div class="footer-item about">
+                        <img class="logo" src="assets/img/logo-light.png" alt="Logo">
+                        <p>
+                            société commerciale à responsabilité limitée, œuvrant dans le domaine agricole.
+                        </p>
+                    </div>
+                </div>
+                <!-- End Single Itme -->
+
+                <!-- Single Itme -->
+                <div class="col-lg-2 col-md-6 item">
+                    <div class="footer-item link">
+                        <h4 class="widget-title">Nos liens</h4>
+                        <ul>
+                            <li>
+                                <a href="about-us.html">Accueil</a>
+                            </li>
+                            <li>
+                                <a href="about-us.html">Mission</a>
+                            </li>
+                            <li>
+                                <a href="team.html">A propos</a>
+                            </li>
+                            <li>
+                                <a href="blog-single-with-sidebar.html">Objectif</a>
+                            </li>
+                            <li>
+                                <a href="services.html">Expertise</a>
+                            </li>
+                            <li>
+                                <a href="team-details.html">Realisations</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- End Single Itme -->
+
+                <!-- Single Itme -->
+                <div class="col-lg-3 col-md-6 item">
+                    <div class="footer-item recent-post">
+                        <h4 class="widget-title">Recent Posts</h4>
+                        <ul>
+                            <li>
+                                <div class="thumb">
+                                    <a href="blog-single-with-sidebar.html">
+                                        <img src="assets/img/thumbs/1.jpg" alt="Thumb">
+                                    </a>
+                                </div>
+                                <div class="info">
+                                    <div class="meta-title">
+                                        <span class="post-date">12 Sep, 2023</span>
+                                    </div>
+                                    <h5><a href="blog-single-with-sidebar.html">Meant widow equal an share least part. </a></h5>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="thumb">
+                                    <a href="blog-single-with-sidebar.html">
+                                        <img src="assets/img/thumbs/2.jpg" alt="Thumb">
+                                    </a>
+                                </div>
+                                <div class="info">
+                                    <div class="meta-title">
+                                        <span class="post-date">18 Jul, 2023</span>
+                                    </div>
+                                    <h5><a href="blog-single-with-sidebar.html">Future Plan & Strategy for Consutruction </a></h5>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- End Single Itme -->
+
+                <!-- Single Itme -->
+                <div class="col-lg-3 col-md-6 item">
+                    <div class="footer-item contact">
+                        <h4 class="widget-title">Contact</h4>
+                        <ul>
+                            <li>
+                                <div class="icon">
+                                    <i class="fas fa-home"></i>
+                                </div>
+                                <div class="content">
+                                    <strong>Adresse :</strong>
+                                    30, boulevard M’siri, C.Kampemba/Lubumbashi.
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fas fa-envelope"></i>
+                                </div>
+                                <div class="content">
+                                    <strong>Email:</strong>
+                                    <a href="mailto:sisinamulimajirdc1@gmail.com">sisinamulimajirdc1@gmail.com</a>
+                                    <br>
+                                    <a href="mailto:ramazanisisinamulimaji@gmail.com">ramazanisisinamulimaji@gmail.com</a>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <i class="fas fa-phone"></i>
+                                </div>
+                                <div class="content">
+                                    <strong>Téléphone:</strong>
+                                    <a href="tel:+243813535732">+243 813535732</a>
+                                    <br>
+                                    <a href="tel:+243993322278">+243 993322278</a>
+                                    <br>
+                                    <a href="tel:+243855729241">+243 855729241</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Single Itme -->
+
+            </div>
+        </div>
+        <!-- Start Footer Bottom -->
+        <div class="footer-bottom text-center">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>&copy; Copyright 2024. Made by  <a href="https://smirltech.com">SmirlTech</a></p>
+                </div>
+            </div>
+        </div>
+        <!-- End Footer Bottom -->
+    </div>
+</footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -801,8 +801,7 @@
         <div class="row">
             <div class="col-lg-8 offset-lg-2">
                 <div class="site-heading text-center">
-                    <h5 class="sub-title">Latest News</h5>
-                    <h2 class="title">Check out our blog posts </h2>
+                    <h2 class="title">Nos réalisations</h2>
                 </div>
             </div>
         </div>
@@ -810,22 +809,6 @@
     <div class="container">
         <div class="row">
             <!-- Single Item -->
-            <div class="col-xl-6 col-md-12 mb-30">
-                <div class="blog-style-one">
-                    <div class="thumb">
-                        <img src="assets/img/blog/1.jpg" alt="Image Not Found">
-                        <div class="overlay text-light">
-                            <h3 class="post-title"><a href="blog-single-with-sidebar.html">Miscovery incommode earnestly commanded if.</a></h3>
-                            <p>
-                                Seeing rather her you not esteem men settle genius excuse. Deal say over you age from. Comparison new ham melancholy son themselves.
-                            </p>
-                            <a href="blog-single-with-sidebar.html" class="button-regular">
-                                Continue Reading <i class="fas fa-arrow-right"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <!-- End Single Item -->
             <!-- Single Item -->
             <div class="col-xl-3 col-md-6 mb-30">
@@ -852,33 +835,6 @@
                     </div>
                 </div>
             </div>
-            <!-- End Single Item -->
-            <!-- Single Item -->
-            <div class="col-xl-3 col-md-6 mb-30">
-                <div class="blog-style-one">
-                    <div class="thumb">
-                        <a href="blog-single-with-sidebar.html"><img src="assets/img/blog/2.jpg" alt="Image Not Found"></a>
-                        <div class="date"><strong>08</strong> <span>Dec, 2023</span></div>
-                    </div>
-                    <div class="info">
-                        <div class="meta">
-                            <ul>
-                                <li>
-                                    <a href="#">Md Sohag</a>
-                                </li>
-                                <li>
-                                    25 April, 2023
-                                </li>
-                            </ul>
-                        </div>
-                        <h3 class="post-title"><a href="blog-single-with-sidebar.html">Considered imprudence of technical friendship.</a></h3>
-                        <a href="blog-single-with-sidebar.html" class="button-regular">
-                            Continue Reading <i class="fas fa-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <!-- End Single Item -->
         </div>
     </div>
 </div>
@@ -886,143 +842,7 @@
 
 <!-- Start Footer
 ============================================= -->
-<footer class="bg-dark text-light" style="background-image: url(assets/img/shape/8.png);">
-    <div class="container">
-        <div class="f-items default-padding">
-            <div class="row">
-
-                <!-- Single Itme -->
-                <div class="col-lg-4 col-md-6 item">
-                    <div class="footer-item about">
-                        <img class="logo" src="assets/img/logo-light.png" alt="Logo">
-                        <p>
-                            Happen active county. Winding morning ambition shyness evident to poor. Because elderly new to the point to main success.
-                        </p>
-                        <form action="#">
-                            <input type="email" placeholder="Your Email" class="form-control" name="email">
-                            <button type="submit"> Go</button>
-                        </form>
-                    </div>
-                </div>
-                <!-- End Single Itme -->
-
-                <!-- Single Itme -->
-                <div class="col-lg-2 col-md-6 item">
-                    <div class="footer-item link">
-                        <h4 class="widget-title">Explore</h4>
-                        <ul>
-                            <li>
-                                <a href="about-us.html">About Us</a>
-                            </li>
-                            <li>
-                                <a href="team.html">Meet Our Team</a>
-                            </li>
-                            <li>
-                                <a href="blog-single-with-sidebar.html">News & Media</a>
-                            </li>
-                            <li>
-                                <a href="services.html">Services</a>
-                            </li>
-                            <li>
-                                <a href="contact-us.html">Contact Us</a>
-                            </li>
-                            <li>
-                                <a href="team-details.html">Volunteers</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End Single Itme -->
-
-                <!-- Single Itme -->
-                <div class="col-lg-3 col-md-6 item">
-                    <div class="footer-item recent-post">
-                        <h4 class="widget-title">Recent Posts</h4>
-                        <ul>
-                            <li>
-                                <div class="thumb">
-                                    <a href="blog-single-with-sidebar.html">
-                                        <img src="assets/img/thumbs/1.jpg" alt="Thumb">
-                                    </a>
-                                </div>
-                                <div class="info">
-                                    <div class="meta-title">
-                                        <span class="post-date">12 Sep, 2023</span>
-                                    </div>
-                                    <h5><a href="blog-single-with-sidebar.html">Meant widow equal an share least part. </a></h5>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="thumb">
-                                    <a href="blog-single-with-sidebar.html">
-                                        <img src="assets/img/thumbs/2.jpg" alt="Thumb">
-                                    </a>
-                                </div>
-                                <div class="info">
-                                    <div class="meta-title">
-                                        <span class="post-date">18 Jul, 2023</span>
-                                    </div>
-                                    <h5><a href="blog-single-with-sidebar.html">Future Plan & Strategy for Consutruction </a></h5>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- End Single Itme -->
-
-                <!-- Single Itme -->
-                <div class="col-lg-3 col-md-6 item">
-                    <div class="footer-item contact">
-                        <h4 class="widget-title">Contact Info</h4>
-                        <ul>
-                            <li>
-                                <div class="icon">
-                                    <i class="fas fa-home"></i>
-                                </div>
-                                <div class="content">
-                                    <strong>Address:</strong>
-                                    5919 Trussville Crossings Pkwy, Birmingham
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="fas fa-envelope"></i>
-                                </div>
-                                <div class="content">
-                                    <strong>Email:</strong>
-                                    <a href="mailto:info@validtheme.com">info@validtheme.com</a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="icon">
-                                    <i class="fas fa-phone"></i>
-                                </div>
-                                <div class="content">
-                                    <strong>Phone:</strong>
-                                    <a href="tel:2151234567">+123 34598768</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Single Itme -->
-
-            </div>
-        </div>
-        <!-- Start Footer Bottom -->
-        <div class="footer-bottom text-center">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>&copy; Copyright 2023. All Rights Reserved by <a href="#">validthemes</a></p>
-                </div>
-            </div>
-        </div>
-        <!-- End Footer Bottom -->
-    </div>
-    <div class="shape-right-bottom">
-        <img src="assets/img/shape/7.png" alt="Image Not Found">
-    </div>
-</footer>
+@include('layouts.footer')
 <!-- End Footer -->
 
 <!-- jQuery Frameworks
