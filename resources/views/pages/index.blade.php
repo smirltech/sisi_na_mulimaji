fafb60e7ebe52e73f21cb80622e8aa9233817144
jean luc kawel <20ka208@esisalama.org>
2024-02-01 10:42:22 +0200

    add logo

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -138,6 +138,90 @@
 <!-- Start Banner Area
 ============================================= -->
 
+<div class="banner-area navigation-circle text-light banner-style-one zoom-effect overflow-hidden">
+    <!-- Slider main container -->
+    <div class="banner-fade">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+
+            <!-- Single Item -->
+            <div class="swiper-slide banner-style-one">
+                <div class="banner-thumb bg-cover shadow dark" style="background: url(assets/img/2024/home.jpeg);"></div>
+                <div class="shape">
+                    <img src="assets/img/shape/2.png" alt="Image Not Found">
+                </div>
+                <div class="container">
+                    <div class="row align-center">
+                        <div class="col-xl-9">
+                            <div class="content">
+                                <div class="badge">
+                                    <div class="curve-text">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150" version="1.1">
+                                            <path id="textPath" d="M 0,75 a 75,75 0 1,1 0,1 z"></path>
+                                            <text><textPath href="#textPath">100% Organic Product</textPath></text>
+                                        </svg>
+                                        <a href="https://www.youtube.com/watch?v=ipUuoMCEbDQ" class="popup-youtube"><i class="fas fa-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="info">
+                                    <h2>Secteurs d’activités</h2>
+                                    <p>
+                                        Cabinet de consultance agricole. Vente des intrants agricoles et Services
+                                    </p>
+                                    <div class="button">
+                                        <a class="btn btn-theme btn-md radius animation" href="about-us.html">Decouvrez plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Item -->
+
+            <!-- Single Item -->
+            <div class="swiper-slide banner-style-one">
+                <div class="banner-thumb bg-cover shadow dark" style="background: url(assets/img/2024/IMG-20230930-WA0101.jpg);"></div>
+                <div class="shape">
+                    <img src="assets/img/shape/2.png" alt="Image Not Found">
+                </div>
+                <div class="container">
+                    <div class="row align-center">
+                        <div class="col-xl-9">
+                            <div class="content">
+                                <div class="badge">
+                                    <div class="curve-text">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150" version="1.1">
+                                            <path id="textPath2" d="M 0,75 a 75,75 0 1,1 0,1 z"></path>
+                                            <text><textPath href="#textPath">100% Organic Product</textPath></text>
+                                        </svg>
+                                        <a href="https://www.youtube.com/watch?v=ipUuoMCEbDQ" class="popup-youtube"><i class="fas fa-arrow-right"></i></a>
+                                    </div>
+                                </div>
+                                <div class="info">
+                                    <h2><strong>Mission</strong></h2>
+                                    <p>
+                                        Accompagner les opérateurs agricole (fermiers, ménages et personnes morales)
+                                    </p>
+                                    <div class="button">
+                                        <a class="btn btn-theme btn-md radius animation" href="#">Decouvrez Plus</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Single Item -->
+
+        </div>
+
+        <!-- Navigation -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+    </div>
+</div>
 
 <!-- End Banner -->
 
@@ -209,7 +293,7 @@
             <div class="col-lg-4 col-md-6 service-one-single">
                 <div class="service-style-one-item">
                     <div class="thumb">
-                        <img src="assets/img/illustration/2.png" alt="Image Not Found">
+                        <img src="assets/img/2024/icon/img_2.png" alt="Image Not Found">
                     </div>
                     <div class="info">
                         <div class="top">
@@ -227,7 +311,7 @@
             <div class="col-lg-4 col-md-6 service-one-single">
                 <div class="service-style-one-item">
                     <div class="thumb">
-                        <img src="assets/img/illustration/3.png" alt="Image Not Found">
+                        <img src="assets/img/2024/icon/img_3.png" alt="Image Not Found">
                     </div>
                     <div class="info">
                         <div class="top">
@@ -244,7 +328,7 @@
             <div class="col-lg-4 col-md-6 service-one-single">
                 <div class="service-style-one-item">
                     <div class="thumb">
-                        <img src="assets/img/illustration/3.png" alt="Image Not Found">
+                        <img src="assets/img/2024/icon/img_4.png" alt="Image Not Found">
                     </div>
                     <div class="info">
                         <div class="top">
